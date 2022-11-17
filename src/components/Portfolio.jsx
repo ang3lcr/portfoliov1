753fa92671dcddf9300235c532ce46fd1a49664b
ang3lcr <ang3lcortes@outlook.com>
2022-11-16 21:36:05 -0800

    fixed links

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,26 +12,38 @@ const Portfolio = () => {
     {
       id: 1,
       src: arrayDestruct,
+      demo: "www.google.com",
+      code: "www.google.com",
     },
     {
       id: 2,
       src: reactParallax,
+      demo: "www.google.com",
+      code: "www.google.com",
     },
     {
       id: 3,
       src: navbar,
+      demo: "www.google.com",
+      code: "www.google.com",
     },
     {
       id: 4,
       src: reactSmooth,
+      demo: "www.google.com",
+      code: "www.google.com",
     },
     {
       id: 5,
       src: installNode,
+      demo: "www.google.com",
+      code: "www.google.com",
     },
     {
       id: 6,
       src: reactWeather,
+      demo: "www.google.com",
+      code: "www.google.com",
     },
   ];
 
@@ -43,13 +55,13 @@ const Portfolio = () => {
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Portfolio
+            Proyectos
           </p>
           <p className="py-6">Check out some of my work right here</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, demo, code }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -58,16 +70,16 @@ const Portfolio = () => {
               />
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
+                  <a href={demo}>Demo</a>
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
+                  <a href={code}>Codigo</a>
                 </button>
               </div>
             </div>
           ))}
         </div>
-      </div>
+      </div> 
     </div>
   );
 };
