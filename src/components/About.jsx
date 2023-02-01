2ca961921d0c2c03e67663d490260ee19687221b
ang3lcr <ang3lcortes@outlook.com>
2023-02-01 10:49:52 -0600

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div name="sobre mi" className='w-full h-screen bg-gradient-to-b from-gray-800 to-purple-900 text-white'>
+        <div name="sobre mi" className='w-full h-screen bg-gradient-to-b from-gray-800 to-gray-900 text-white'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
                 </div>
-                <p className='text-xl mt-20'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure distinctio expedita veritatis ipsam odit maiores exercitationem consectetur cum eligendi, quia assumenda nulla enim porro inventore quae earum dolorum laborum voluptatibus veniam corrupti natus consequatur! Alias rem deleniti provident ipsam, odit quaerat sequi exercitationem ducimus ut, illum tempore praesentium voluptas corrupti?</p>
-                <br />
-                <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, ea esse. Doloribus ex porro non! Itaque iste quidem suscipit natus tempora id aperiam nam et nihil, est veritatis officia iure debitis nulla repudiandae maxime obcaecati non. Sint debitis nemo exercitationem dicta sequi odio at vel deleniti. Totam cumque distinctio beatae?</p>
+                <p className='text-xl mt-20'>"As a Junior Web Developer, I bring a passion for creating dynamic and visually appealing websites. I have a solid understanding of HTML, CSS, and React, and I'm always seeking to expand my knowledge and improve my skills. I possess strong attention to detail, ensuring that all aspects of the website function smoothly and meet the client's specifications. I work well in a team and am able to effectively communicate my ideas and vision to bring the project to life. My goal is to build websites that not only look great but provide a seamless user experience. Let's create something amazing together."</p>
+                <br/>
+
+
+
+
+                
             </div>
         </div>
     )
